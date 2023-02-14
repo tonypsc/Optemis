6d1dd73125a8e2d4a9d81ffe47cb056044dd151b
tonypsc <tonyps@gmail.com>
2023-02-14 13:20:30 -0500

    Add Suspense and React query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { Suspense } from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryErrorResetBoundary,
+} from 'react-query';
+
 import { Home } from './home';
+import { LoadingView } from './shared';
 
 import './App.css';
 
 function App() {
-  return <Home />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<LoadingView />}>
+        <Home />
+      </Suspense>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
